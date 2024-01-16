203b4a9c94ed18afaf4b7672802847bdf4c06c88
Anarion <anarion80@gmail.com>
2024-01-16 12:01:52 +0100

    ⬆️ Update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import image from "@astrojs/image";
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -8,7 +7,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'http://localhost/',
-	integrations: [react(), sitemap(), image({
-		serviceEntryPoint: '@astrojs/image/sharp'
-	})],
+	integrations: [react(), sitemap()],
 });
